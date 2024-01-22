67709e617e294c1de60be7ceab9fa5764119cff3
KirrBrest <lukashchuk.kiryl@yandex.by>
2024-01-23 02:56:19 +0300

    feat: 5 and start 6 (Tue, Jan 23, 2024 02:56:19 AM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -11,25 +11,29 @@ fs.readdir(dataStyle, { withFileTypes: true }, (err, files) => {
   } else {
     files.forEach((file) => {
       if (file.isFile()) {
-        const filePath = path.join(dataStyle, file);
-        const type = path.extname(filePath);
-        if (type === '.css') {
+        // const filePath = path.join(dataStyle, file);
+        // const type = path.extname(filePath);
+        const filePath = file.name.toString();
+        const type = file.name.toString().split('.')[1];
+        if (type === 'css') {
           // const input = fs.createReadStream(filePath);
           // input.on('data', (data) => {
           //   result.write(data + '\n');
           fs.readFile(
             path.join(__dirname, 'styles', filePath),
             'utf-8',
-            (data) => {
+            (err, data) => {
+              if (err) throw err;
               const arrStyles = [];
               const style = data.toString();
               arrStyles.push(style);
               for (let i = 0; i < arrStyles.length; i++) {
-                output.write(arrStyles[i]);
+                result.write(arrStyles[i]);
               }
             }
           );
         }
-    }
-  });
+      }
+    });
+  }
 });
